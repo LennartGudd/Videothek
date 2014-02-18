e9b536632616f96fc9c898634f8d62f0a3e2f69b
Antonios K <mtxcm22@gmail.com>
2014-02-18 19:17:25 +0100

    Modifying Tables

--- app/models/film.rb
+++ app/models/film.rb
@@ -3,10 +3,14 @@ has_many :film_categories
 has_many :categories, through: :film_categories
 
 has_many :comments
+has_many :users, through: :comments
 
 has_many :film_actors
 has_many :actors, through: :film_actors
 
 has_many :film_directors
 has_many :directors, through: :film_directors
+
+has_many :rentals
+has_many :users, through: :rentals
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Videothek::Application.routes.draw do
+  devise_for :users
   resources :comments
 
   resources :films
