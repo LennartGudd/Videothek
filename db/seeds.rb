21c4d18c1abf4bdb6e6b28edf985c63094ce6bfe
Lennart <lennartgudd@fh-muenster.de>
2014-02-25 21:07:10 +0100

    ?

--- db/seeds.rb
+++ db/seeds.rb
@@ -178,311 +178,338 @@ comment5.save!
 actor = Actor.new
 actor.id = '1'
 actor.name = 'Tim Robbins'
+actor.film_ids = [film.id]
 actor.save!
-filmactor = Filmactor
-filmactor.film_id = '1'
-filmactor.actor_id = '1'
-filmactor.save!
+#film_actor = Film_actor.new
+#film_actor.id = '1'
+#film_actor.film_id = '1'
+#film_actor.actor_id = '1'
+#film_actor.save!
 
 actor2 = Actor.new
 actor2.id = '2'
 actor2.name = 'Morgan Freeman'
+actor2.film_ids = [film.id]
 actor2.save!
-filmactor2 = Filmactor
-filmactor2.film_id = '1'
-filmactor2.actor_id = '2'
-filmactor2.save!
+#film_actor2 = Film_actor
+#film_actor2.film_id = '1'
+#film_actor2.actor_id = '2'
+#film_actor2.save!
 
 actor3 = Actor.new
 actor3.id = '3'
 actor3.name = 'Bob Gunton'
+actor3.film_ids = [film.id]
 actor3.save!
-filmactor3 = Filmactor
-filmactor3.film_id = '1'
-filmactor3.actor_id = '3'
-filmactor3.save!
+#film_actor3 = Film_actor
+#film_actor3.film_id = '1'
+#film_actor3.actor_id = '3'
+#film_actor3.save!
 
 actor4 = Actor.new
 actor4.id = '4'
 actor4.name = 'Marlon Brando'
+actor4.film_ids = [film2.id]
 actor4.save!
-filmactor4 = Filmactor
-filmactor4.film_id = '2'
-filmactor4.actor_id = '4'
-filmactor4.save!
+#film_actor4 = Film_actor
+#film_actor4.film_id = '2'
+#film_actor4.actor_id = '4'
+#film_actor4.save!
 
 actor5 = Actor.new
 actor5.id = '5'
 actor5.name = 'Al Pacino'
+actor5.film_ids = [film2.id]
 actor5.save!
-filmactor5 = Filmactor
-filmactor5.film_id = '2'
-filmactor5.film_id = '5'
-filmactor5.save!
-filmactor6 = Filmactor
-filmactor6.film_id = '3'
-filmactor6.actor_id = '5'
-filmactor6.save!
+
+#film_actor5 = Film_actor
+#film_actor5.film_id = '2'
+#film_actor5.film_id = '5'
+#film_actor5.save!
+#film_actor6 = Film_actor
+#film_actor6.film_id = '3'
+#film_actor6.actor_id = '5'
+#film_actor6.save!
 
 actor6 = Actor.new
 actor6.id = '6'
 actor6.name = 'Robert De Niro'
+actor6.film_ids = [film3.id]
 actor6.save!
-filmactor7 = Filmactor
-filmactor7.film_id = '3'
-filmactor7.actor_id = '6'
-filmactor7.save!
+#film_actor7 = Film_actor
+#film_actor7.film_id = '3'
+#film_actor7.actor_id = '6'
+#film_actor7.save!
 
 actor7 = Actor.new
 actor7.id = '7'
 actor7.name = 'Christian Bale'
+actor7.film_ids = [film4.id]
 actor7.save!
-filmactor8 = Filmactor
-filmactor8.film_id = '4'
-filmactor8.actor_id = '7'
-filmactor8.save!
+#film_actor8 = Film_actor
+#film_actor8.film_id = '4'
+#film_actor8.actor_id = '7'
+#film_actor8.save!
 
 actor8 = Actor.new
 actor8.id = '8'
 actor8.name = 'Heath Ledger'
+actor8.film_ids = [film4.id]
 actor8.save!
-filmactor9 = Filmactor
-filmactor9.film_id = '4'
-filmactor9.actor_id = '8'
-filmactor9.save!
+#film_actor9 = Film_actor
+#film_actor9.film_id = '4'
+#film_actor9.actor_id = '8'
+#film_actor9.save!
 
 actor9 = Actor.new
 actor9.id = '9'
 actor9.name = 'John Travolta'
+actor9.film_ids = [film5.id]
 actor9.save!
-filmactor10 = Filmactor
-filmactor10.film_id = '5'
-filmactor10.actor_id = '9'
-filmactor10.save!
+#film_actor10 = Film_actor
+#film_actor10.film_id = '5'
+#film_actor10.actor_id = '9'
+#film_actor10.save!
 
 actor10 = Actor.new
 actor10.id = '10'
 actor10.name = 'Uma Thurman'
+actor10.film_ids = [film5.id]
 actor10.save!
-filmactor11 = Filmactor
-filmactor11.film_id = '5'
-filmactor11.actor_id = '10'
-filmactor11.save!
+#film_actor11 = Film_actor
+#film_actor11.film_id = '5'
+#film_actor11.actor_id = '10'
+#film_actor11.save!
 
 #regisseur--------------------------------------------------------
 director = Director.new
 director.id = '1'
 director.name = 'Frank Darabont'
+director.film_ids = [film.id]
 director.save!
-filmdirector = Filmdirector.new
-filmdirector.film_id = '1'
-filmdirector.director_id = '1'
-filmdirector.save!
+#film_director = Film_director.new
+#film_director.film_id = '1'
+#film_director.director_id = '1'
+#film_director.save!
 
 director2 = Director.new
-regisseur2.id = '2'
-regisseur2.name = 'Francis Ford Coppola'
-regisseur2.save!
-filmdirector2 = Filmdirector.new
-filmdirector2.film_id = '2'
-filmdirector2.director_id = '2'
-filmdirector2.save!
-filmdirector3 = Filmdirector.new
-filmdirector3.film_id = '3'
-filmdirector3.director_id = '2'
-filmdirector3.save!
+director2.id = '2'
+director2.name = 'Francis Ford Coppola'
+director2.film_ids = [film2.id]
+director2.save!
+#film_director2 = Film_director.new
+#film_director2.film_id = '2'
+#film_director2.director_id = '2'
+#film_director2.save!
+#film_director3 = Film_director.new
+#film_director3.film_id = '3'
+#film_director3.director_id = '2'
+#film_director3.save!
 
 director3 = Director.new
 director3.id = '3'
 director3.name = 'Christopher Nolan'
+director3.film_ids = [film4.id]
 director3.save!
-filmdirector4 = Filmdirector.new
-filmdirector4.film_id = '4'
-filmdirector4.director_id = '3'
-filmdirector4.save!
+#film_director4 = Film_director.new
+#film_director4.film_id = '4'
+#film_director4.director_id = '3'
+#film_director4.save!
 
 director4 = Director.new
 director4.id = '4'
 director4.name = 'Quentin Tarantino'
+director4.film_ids = [film5.id]
 director4.save!
-filmdirector5 = Filmdirector.new
-filmdirector5.film_id = '5'
-filmdirector5.director_id = '4'
-filmdirector5.save!
+#film_director5 = Film_director.new
+#film_director5.film_id = '5'
+#film_director5.director_id = '4'
+#film_director5.save!
 
 director5 = Director.new
 director5.id = '5'
 director5.name = 'Sergio Leone'
+director5.film_ids = [film6.id]
 director5.save!
-filmdirector6 = Filmdirector.new
-filmdirector6.film_id = '6'
-filmdirector6.director_id = '5'
-filmdirector6.save!
+#film_director6 = Film_director.new
+#film_director6.film_id = '6'
+#film_director6.director_id = '5'
+#film_director6.save!
 
 director6 = Director.new
 director6.id = '6'
 director6.name = 'Steven Spielberg'
+director6.film_ids = [film7.id]
 director6.save!
-filmdirector7 = Filmdirector.new
-filmdirector7.film_id = '7'
-filmdirector7.director_id = '6'
-filmdirector7.save!
+#film_director7 = Film_director.new
+#film_director7.film_id = '7'
+#film_director7.director_id = '6'
+#film_director7.save!
 
 #category---------------------------------------------------------
 category = Category.new
 category.id = '1'
-category.name = 'Crime'
+category.genre = 'Crime'
+category.film_ids = [film.id]
 category.save!
 
 category2 = Category.new
 category2.id = '2'
-category2.name = 'Drama'
+category2.genre = 'Drama'
+category2.film_ids = [film2.id]
 category2.save!
 
 category3 = Category.new
 category3.id = '3'
-category3.name = 'Action'
+category3.genre = 'Action'
+category3.film_ids = [film4.id]
 category3.save!
 
 category4 = Category.new
 category4.id = '4'
-category4.name = 'Thriller'
+category4.genre = 'Thriller'
+category4.film_ids = [film5.id]
 category4.save!
 
 category5 = Category.new
 category5.id = '5'
-category5.name = 'Adventure'
+category5.genre = 'Adventure'
+category5.film_ids = [film6.id]
 category5.save!
 
 category6 = Category.new
 category6.id = '6'
-category6.name = 'Western'
+category6.genre = 'Western'
+category6.film_ids = [film6.id]
 category6.save!
 
 category7 = Category.new
 category7.id = '7'
-category7.name = 'Biography'
+category7.genre = 'Biography'
+category7.film_ids = [film7.id]
 category7.save!
 
 category8 = Category.new
 category8.id = '8'
-category8.name = 'History'
+category8.genre = 'History'
+category8.film_ids = [film7.id]
 category8.save!
 
 category9 = Category.new
 category9.id = '9'
-category9.name = 'Fantasy'
+category9.genre = 'Fantasy'
+category9.film_ids = [film9.id]
 category9.save!
 
-filmcategory = Filmcategory.new
-filmcategory.film_id = '1'
-filmcategory.category_id = '1'
-filmcategory.save!
+#film_category = Filmcategory.new
+#film_category.film_id = '1'
+#film_category.category_id = '1'
+#film_category.save!
 
-filmcategory2 = Filmcategory.new
-filmcategory2.film_id = '1'
-filmcategory2.category_id = '2'
-filmcategory2.save!
+#film_category2 = Filmcategory.new
+#film_category2.film_id = '1'
+#film_category2.category_id = '2'
+#film_category2.save!
 
-filmcategory3 = Filmcategory.new
-filmcategory3.film_id = '2'
-filmcategory3.category_id = '1'
-filmcategory3.save!
+#film_category3 = Filmcategory.new
+#film_category3.film_id = '2'
+#film_category3.category_id = '1'
+#film_category3.save!
 
-filmcategory4 = Filmcategory.new
-filmcategory4.film_id = '2'
-filmcategory4.category_id = '2'
-filmcategory4.save!
+#film_category4 = Filmcategory.new
+#film_category4.film_id = '2'
+#film_category4.category_id = '2'
+#film_category4.save!
 
-filmcategory5 = Filmcategory.new
-filmcategory5.film_id = '3'
-filmcategory5.category_id = '1'
-filmcategory5.save!
+#film_category5 = Filmcategory.new
+#film_category5.film_id = '3'
+#film_category5.category_id = '1'
+#film_category5.save!
 
-filmcategory6 = Filmcategory.new
-filmcategory6.film_id = '3'
-filmcategory6.category_id = '2'
-filmcategory6.save!
+#film_category6 = Filmcategory.new
+#film_category6.film_id = '3'
+#film_category6.category_id = '2'
+#film_category6.save!
 
-filmcategory7 = Filmcategory.new
-filmcategory7.film_id = '4'
-filmcategory7.category_id = '1'
-filmcategory7.save!
+#film_category7 = Filmcategory.new
+#film_category7.film_id = '4'
+#film_category7.category_id = '1'
+#film_category7.save!
 
-filmcategory8 = Filmcategory.new
-filmcategory8.film_id = '4'
-filmcategory8.category_id = '2'
-filmcategory8.save!
+#film_category8 = Filmcategory.new
+#film_category8.film_id = '4'
+#film_category8.category_id = '2'
+#film_category8.save!
 
-filmcategory9 = Filmcategory.new
-filmcategory9.film_id = '4'
-filmcategory9.category_id = '3'
-filmcategory9.save!
+#film_category9 = Filmcategory.new
+#film_category9.film_id = '4'
+#film_category9.category_id = '3'
+#film_category9.save!
 
-filmcategory10 = Filmcategory.new
-filmcategory10.film_id = '5'
-filmcategory10.category_id = '1'
-filmcategory10.save!
+#film_category10 = Filmcategory.new
+#film_category10.film_id = '5'
+#film_category10.category_id = '1'
+#film_category10.save!
 
-filmcategory11 = Filmcategory.new
-filmcategory11.film_id = '5'
-filmcategory11.category_id = '2'
-filmcategory11.save!
+#film_category11 = Filmcategory.new
+#film_category11.film_id = '5'
+#film_category11.category_id = '2'
+#film_category11.save!
 
-filmcategory12 = Filmcategory.new
-filmcategory12.film_id = '5'
-filmcategory12.category_id = '4'
-filmcategory12.save!
+#film_category12 = Filmcategory.new
+#film_category12.film_id = '5'
+#film_category12.category_id = '4'
+#film_category12.save!
 
-filmcategory13 = Filmcategory.new
-filmcategory13.film_id = '6'
-filmcategory13.category_id = '5'
-filmcategory13.save!
+#film_category13 = Filmcategory.new
+#film_category13.film_id = '6'
+#film_category13.category_id = '5'
+#film_category13.save!
 
-filmcategory14 = Filmcategory.new
-filmcategory14.film_id = '6'
-filmcategory14.category_id = '6'
-filmcategory14.save!
+#film_category14 = Filmcategory.new
+#film_category14.film_id = '6'
+#film_category14.category_id = '6'
+#film_category14.save!
 
-filmcategory15 = Filmcategory.new
-filmcategory15.film_id = '7'
-filmcategory15.category_id = '7'
-filmcategory15.save!
+#film_category15 = Filmcategory.new
+#film_category15.film_id = '7'
+#film_category15.category_id = '7'
+#film_category15.save!
 
-filmcategory16 = Filmcategory.new
-filmcategory16.film_id = '7'
-filmcategory16.category_id = '8'
-filmcategory16.save!
+#film_category16 = Filmcategory.new
+#film_category16.film_id = '7'
+#film_category16.category_id = '8'
+#film_category16.save!
 
-filmcategory17 = Filmcategory.new
-filmcategory17.film_id = '7'
-filmcategory17.category_id = '2'
-filmcategory17.save!
+#film_category17 = Filmcategory.new
+#film_category17.film_id = '7'
+#film_category17.category_id = '2'
+#film_category17.save!
 
-filmcategor18 = Filmcategory.new
-filmcategory18.film_id = '8'
-filmcategory18.category_id = '2'
-filmcategory18.save!
+#film_categor18 = Filmcategory.new
+#film_category18.film_id = '8'
+#film_category18.category_id = '2'
+#film_category18.save!
 
-filmcategory19 = Filmcategory.new
-filmcategory19.film_id = '9'
-filmcategory19.category_id = '9'
-filmcategory19.save!
+#film_category19 = Filmcategory.new
+#film_category19.film_id = '9'
+#film_category19.category_id = '9'
+#film_category19.save!
 
-filmcategory20 = Filmcategory.new
-filmcategory20.film_id = '9'
-filmcategory20.category_id = '5'
-filmcategory20.save!
+#film_category20 = Filmcategory.new
+#film_category20.film_id = '9'
+#film_category20.category_id = '5'
+#film_category20.save!
 
-filmcategory21 = Filmcategory.new
-filmcategory21.film_id = '9'
-filmcategory21.category_id = '3'
-filmcategory21.save!
+#film_category21 = Filmcategory.new
+#film_category21.film_id = '9'
+#film_category21.category_id = '3'
+#film_category21.save!
 
-filmcategory22 = Filmcategory.new
-filmcategory22.film_id = '10'
-filmcategory22.category_id = '2'
-filmcategory22.save!
+#film_category22 = Filmcategory.new
+#film_category22.film_id = '10'
+#film_category22.category_id = '2'
+#film_category22.save!
 
 #ratings not able to write constant rating_cache-----------------
 #rate = Rate.new
